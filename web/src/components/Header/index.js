51a98ffde3b46404e501900e9272cd8e53e91a04
monteirovictor <victormonteiromaia95@gmail.com>
2020-12-26 16:25:29 -0300

    26-12-2020

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import * as S from './styles';
+
+import logo from '../../assets/logo.png';
 
 function Header() {
   return (
-    <h1>Header </h1>
+    <S.Container>
+      <S.LeftSide>
+        <img src={logo} alt="Logo"/>
+      </S.LeftSide>
+      <S.RightSide>
+        <a href="">Inicio</a>
+        <a href="">Nova Tarefa</a>
+        <a href="">Sincronizar Celular</a>
+       
+      </S.RightSide>
+    </S.Container>
   );
 }
 
--- web/src/components/Header/styles.js
+++ web/src/components/Header/styles.js
@@ -1,1 +1,45 @@
 import styled from 'styled-components';
+
+export const Container =  styled.div`
+width:100%;
+height:70px;
+background:#20265F;
+border-bottom: 5px solid #EE6B26;
+display:flex;
+`;
+
+export const LeftSide =  styled.div`
+
+width:50%;
+height:50px;
+display:flex;
+align-items:center;
+padding-left:5px;
+
+img{
+    width:100px;
+    height:40px;
+}
+
+`;
+
+export const RightSide =  styled.div`
+
+width:50%;
+height:50px;
+display:flex;
+align-items:center;
+justify-content:flex-end;
+
+a{
+    color:#fff;
+    font-weight:bold;
+    text-decoration:none;
+    margin:0 10px;
+    &:hover{
+        color:#EE6B26;
+    }
+}
+
+
+`;
